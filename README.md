ed8d8b8fc56dc112eadc374cec3ac989a07cee94
Neil MacGregor <nmacgreg@ualberta.ca>
2022-08-31 04:29:40 -0600

    Ran this under F36, and it suddenly works

--- README.md
+++ README.md
@@ -4,12 +4,21 @@
 * The idea is to make it easy to review the collected footage, with features like queueing the ones you haven't seen, and allowing you to tag interesting videos. 
 
 <hr>
-User Guide: 
+##User Guide: 
+
 * Setup: 
     * Use this environment variable to control where this software will search for files: `export VIDEOPATH="/home/nmacgreg/Videos/Surveillance"`
 * Run the script: `python grokFiles.py`
 
 ## Take #2
 
-* Start redis in a container
+* Start redis in a container: `podman run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest`
+* Confirm you can connect to it: `podman exec -it --rm redis-stack redis-cli`
 * `python dbRedis.py` will query a list, from redis
+
+## Take #3
+
+* new main.py
+* `pipenv install FastAPI uvicorn jinja2`
+* `uvicorn main:app --reload` in Dev, anyway
+
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import redis
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 r = redis.Redis()
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 #########################################################################################
 @app.get("/")
@@ -14,9 +16,9 @@ async def root():
     return {"message": "Hello World"}
 
 #########################################################################################
-@app.get("/review")
-async def root():
-    """This function populates a template"""
-    return {"message": "Hello World"}
-
+@app.get("/review", response_class=HTMLResponse)
+async def review_video(request: Request, video_URI: str):
+    """This function populates a template with the filename of the video"""
+    video_URI = "2-13-20220828232950.mp4" # canned example
+    return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
 

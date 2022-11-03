64c3cc47c7a21186a81dc1f0bf3cfa59c2bf6af1
Neil MacGregor <neil.macgregor@ualberta.ca>
2022-11-03 10:01:29 -0600

    Added new feature - the 'reviewVideos' template displays the number of videos remaining in the queue

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ baseURI = str(os.getenv('baseURI'))
 redisHost = str(os.getenv('redisHost'))
 
 r = redis.Redis(host=redisHost)
+RedisKey="FilesToReview"
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -19,7 +20,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def root(request: Request):
     """This function queries the top of the Redis queue, and uses the URL found there to feed a video into the player """
-    files = r.lrange("FilesToReview", 0, 2)
+    files = r.lrange(RedisKey, 0, 2)
     if files:
         return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": files[0].decode('UTF-8')})
     else: 
@@ -31,17 +32,17 @@ async def review_video(request: Request, video_file: str):
     """This function populates a template with the filename of the video"""
     #video_URI = baseURI + video_file # canned example
     video_URI = video_file # canned example
-    return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
+    return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI, "videos_Remaining": r.llen(RedisKey)})
 
 #########################################################################################
 @app.get("/Finished", response_class=HTMLResponse)
 async def finished_review(request: Request):
     """This function first lpops the list in redis, queries the next file in redis, and populates a template with the filename of the video"""
-    r.lpop("FilesToReview")
-    video_file = r.lrange ("FilesToReview", 0, 1)
+    r.lpop(RedisKey)
+    video_file = r.lrange (RedisKey, 0, 1)
     if video_file: 
         video_URI = video_file[0].decode('UTF-8') # 
-        return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
+        return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI, "videos_Remaining": r.llen(RedisKey)})
     else: 
         return templates.TemplateResponse("allDone.html", {"request": request })
 
@@ -51,4 +52,4 @@ async def add_tag(request: Request, video_file: str, tag: str):
     """This function adds a tag to a filename"""
     r.rpush(tag, video_file)
     video_URI = baseURI + video_file
-    return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
+    return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI, "videos_Remaining": r.llen(RedisKey)})

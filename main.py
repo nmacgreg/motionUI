a897325258106df39e584279cc87f4056c8bd70d
Neil MacGregor <neil.macgregor@ualberta.ca>
2022-11-03 09:13:38 -0600

    main.py now re-selects the top record from Redis every time you reload it

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 baseURI = str(os.getenv('baseURI'))
+redisHost = str(os.getenv('redisHost'))
 
-r = redis.Redis()
-files = r.lrange("FilesToReview", 0, 2)
+r = redis.Redis(host=redisHost)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -19,13 +19,15 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def root():
     """This function merely returns a Hello World dict"""
+    files = r.lrange("FilesToReview", 0, 2)
     return {"message": "The first file is: " + files[0].decode('UTF-8')}
 
 #########################################################################################
 @app.get("/review", response_class=HTMLResponse)
 async def review_video(request: Request, video_file: str):
     """This function populates a template with the filename of the video"""
-    video_URI = baseURI + video_file # canned example
+    #video_URI = baseURI + video_file # canned example
+    video_URI = video_file # canned example
     return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
 
 #########################################################################################
@@ -34,7 +36,8 @@ async def finished_review(request: Request):
     """This function first lpops the list in redis, queries the next file in redis, and populates a template with the filename of the video"""
     r.lpop("FilesToReview")
     video_file = r.lrange ("FilesToReview", 0, 1)
-    video_URI = baseURI + video_file[0].decode('UTF-8') # 
+    #video_URI = baseURI + video_file[0].decode('UTF-8') # 
+    video_URI = video_file[0].decode('UTF-8') # 
     return templates.TemplateResponse("reviewVideos.html", {"request": request, "video_URI": video_URI})
 
 #########################################################################################
